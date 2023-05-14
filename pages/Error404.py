a6d11f9ef7668017556b02b13000116879e95ba3
Zhi Qiang Quek <zhi_qiang_quek@aiap.sg>
2023-05-14 17:54:59 +0800

    Added multipages template

---
+++ pages/Error404.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+
+def student_page():
+    st.set_page_config(
+    layout = "wide",
+    initial_sidebar_state = 'expanded'
+    )
+
+
+    st.title("Access Denied. You do not have permissions to view. ")
