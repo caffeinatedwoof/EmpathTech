604154dd003cfd5c5a1c9c20491d3677e1980a80
Zhi Qiang Quek <zhi_qiang_quek@aiap.sg>
2023-05-14 20:30:39 +0800

    Added sample pages (pending further rework)

---
+++ pages/student_view_journal.py
@@ -0,0 +1,18 @@
+import streamlit as st
+
+
+def student_page():
+    db = st.session_state.db
+
+    if "selected_student" not in st.session_state:
+        st.title("Student not yet selected")
+
+    elif st.session_state.selected_student:
+        selected_student = st.session_state.selected_student
+        st.title(f"Journal Entries for {selected_student['name']}")
+        entries = db.get_journal_entries(selected_student['_id'])
+        for entry in entries:
+            st.markdown(f"**{entry['date']}**")
+            st.markdown(f"{entry['title']}")
+            st.markdown(f"{entry['content']}")
+            st.markdown(f"---")
