8e39cd086759eeea6456a3c8c9e7fae3821dd305
Zhi Qiang Quek <zhi_qiang_quek@aiap.sg>
2023-05-14 20:22:02 +0800

    Batch script to facilitate starting of streamlit service

---
+++ start_streamlit.bat
@@ -0,0 +1,7 @@
+
+echo off
+echo "Please ensure that you have created an conda environment named empathtech in order to start streamlit. Else expect errors"
+echo "Activate empathtech env"
+call activate empathtech
+
+call streamlit run home.py
