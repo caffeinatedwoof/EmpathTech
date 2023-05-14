a3766e6aed5755c7cd94eb4fdca851c9c7727bd4
Zhi Qiang Quek <zhi_qiang_quek@aiap.sg>
2023-05-14 20:19:38 +0800

    Updated with a simple button allowing main login page redirect on top of 404 error display

--- pages/Error404.py
+++ pages/Error404.py
@@ -1,11 +1,17 @@
 import streamlit as st
+from st_helper_func import remove_top_space_canvas, navbar_edit
+from streamlit_extras.switch_page_button import switch_page
 
-
-def student_page():
-    st.set_page_config(
-    layout = "wide",
+# Layout config 
+st.set_page_config(
+    layout = "centered",
     initial_sidebar_state = 'expanded'
-    )
+)
+remove_top_space_canvas()
+navbar_edit()
 
 
-    st.title("Access Denied. You do not have permissions to view. ")
+st.title("Access Denied. You do not have permissions to view. ")
+
+# Display a button to go back home
+st.button("Click here to go to login page", on_click=switch_page("home"))
