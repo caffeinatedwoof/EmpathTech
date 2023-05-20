6bdce62867d8244f20ca905056335359a41f64cd
Zhi Qiang Quek <zhi_qiang_quek@aiap.sg>
2023-05-20 23:01:42 +0800

    Created table and removed unwanted contents

--- pages/4_Teaching_classes.py
+++ pages/4_Teaching_classes.py
@@ -1,5 +1,6 @@
 import streamlit as st
-from st_helper_func import remove_top_space_canvas, navbar_edit, hide_student_pages, display_logout_button, error_page_redirect, connect_db
+import pandas as pd
+from st_helper_func import remove_top_space_canvas, navbar_edit, hide_student_pages, error_page_redirect, connect_db
 #from streamlit_extras.switch_page_button import switch_page
 
 # Layout config 
@@ -14,39 +15,54 @@ remove_top_space_canvas()
 navbar_edit()
 hide_student_pages()
 
+@st.cache_data
+def show_student_filter():
+    return [student for student in db.get_all_students(teacher_id)]
+
 #Temporary hack
-st.session_state.logged_in = True
+#st.session_state.logged_in = True
 
 if 'logged_in' in st.session_state and st.session_state.logged_in:
-
-    display_logout_button()
-    st.title("Teaching Classes")
-
-    # Initialize variables
+    # Initialize variables from sessions
     teacher_id = st.session_state.role_id
     teacher_name = st.session_state.user_fullname
+    teaching_class = st.session_state.teaching_class
 
-    @st.cache_data
-    def show_student_filter():
-        return [student for student in db.get_all_students(teacher_id)]
+    st.title(f"Hi {teacher_name}, Teaching classes for this year")
 
     if 'db' in st.session_state:
         db = st.session_state.db
     else:
         db = connect_db()
 
-    st.markdown(f"You have logged in! :) {teacher_name}")
     students = db.students
     student_list = show_student_filter()
-    student_names = [student['name'] for student in student_list]
-    selected_student_name = st.selectbox("Your students", student_names)
 
-    for student in student_list:
-        if student['name'] == selected_student_name:
-            selected_student = student
-            st.session_state.selected_student = selected_student
-            break
+    total_students = len(student_list)
+    year_head = 'Pending information'
+    role = 'Form teacher'
+    subject = 'English'
+
+    # Put info into dictionary
+    teaching_info_dict = {
+        'Class taught': [teaching_class],
+        'Role': [role],
+        'Year Head': [year_head],
+        'Subject': [subject],
+        'Total Students': [len(student_list)]
+    }
+
+    df = pd.DataFrame(teaching_info_dict)
+    hide_table_row_index = """
+                <style>
+                thead tr th:first-child {display:none}
+                tbody th {display:none}
+                </style>
+                """
+
+    # Inject CSS with Markdown
+    st.markdown(hide_table_row_index, unsafe_allow_html=True)
+    st.table(df)
 
 else:
-    pass
-    #error_page_redirect()
+    error_page_redirect()
