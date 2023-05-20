14aa4f70182c4cf86b5c15680ae0f8f4c0265e46
Zhi Qiang Quek <zhi_qiang_quek@aiap.sg>
2023-05-20 10:58:02 +0800

    Renamed files to better reflect its purpose and included display of logout button and hiding of unnecessary files

---
+++ pages/1_Create_journal.py
@@ -0,0 +1,33 @@
+import streamlit as st
+from st_helper_func import remove_top_space_canvas, navbar_edit, hide_teacher_pages, display_logout_button
+
+#from streamlit_extras.switch_page_button import switch_page
+
+# Layout config 
+st.set_page_config(
+    layout = "wide",
+    initial_sidebar_state = 'expanded'
+)
+remove_top_space_canvas()
+navbar_edit()
+hide_teacher_pages()
+
+st.session_state.update(st.session_state)
+
+db = st.session_state.db
+
+# Case when user is logged in
+display_logout_button()
+
+if "selected_student" not in st.session_state:
+    st.title("Student not yet selected")
+
+elif st.session_state.selected_student:
+    selected_student = st.session_state.selected_student
+    st.title(f"Journal Entries for {selected_student['name']}")
+    entries = db.get_journal_entries(selected_student['_id'])
+    for entry in entries:
+        st.markdown(f"**{entry['date']}**")
+        st.markdown(f"{entry['title']}")
+        st.markdown(f"{entry['content']}")
+        st.markdown(f"---")
