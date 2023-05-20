8947db0cc788e6329a6404fb4533561d963d48f3
Zhi Qiang Quek <zhi_qiang_quek@aiap.sg>
2023-05-20 14:02:40 +0800

    Updated logic and switch page after showing access deny

--- pages/6_Access_denied.py
+++ pages/6_Access_denied.py
@@ -1,5 +1,7 @@
 import streamlit as st
-from st_helper_func import remove_top_space_canvas, navbar_edit, hide_student_pages, hide_teacher_pages
+import os
+import toml
+from st_helper_func import remove_top_space_canvas, navbar_edit, reset_session_state, clear_all_but_first_page
 from streamlit_extras.switch_page_button import switch_page
 
 # Layout config 
@@ -10,15 +12,17 @@ st.set_page_config(
 remove_top_space_canvas()
 navbar_edit()
 
-LOGIN_PAGE = "home.py"
-hide_student_pages()
-hide_teacher_pages()
-st.session_state.update(st.session_state)
+data = toml.load(os.path.join('.streamlit','pages.toml'))
+
+LOGIN_PAGE = data['login_path_name']['name']
+
+reset_session_state()
+
+# Remove navbar on load
+clear_all_but_first_page()
 
 #Internally, Streamlit manages two different states : user-defined states (used when you store values like so: st.session_state.my_state = "hey"), and widget states (when you use a key parameter). These two states work a little bit differently. User-defined states are completely persistent after multiple runs. However if a widget with a key assigned disappear (when your page changes for example), its associated widget state will be cleared. So to make widget state persistent, the trick is to transform a widget state into a user-defined state. 
-st.session_state.update(st.session_state)
-
-st.title("Access Denied. You do not have permissions to view. ")
+st.title("Access Denied. You are not logged in or you do not have permissions to view.")
 
 # Display a button to go back home
-st.button("Click here to go to login page", on_click=switch_page(LOGIN_PAGE))
+switch_page(LOGIN_PAGE)
