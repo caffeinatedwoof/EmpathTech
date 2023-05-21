33f9c139a05260d4896a14c0a6f4879a2a4c9986
Zhi Qiang Quek <zhi_qiang_quek@aiap.sg>
2023-05-21 21:47:22 +0800

    Removed unnecessary codes

--- pages/1_Create_journal.py
+++ pages/1_Create_journal.py
@@ -20,18 +20,5 @@ if 'logged_in' in st.session_state and st.session_state.logged_in:
     else:
         db = connect_db()
 
-    if "selected_student" not in st.session_state:
-        st.title("Student not yet selected")
-
-    elif st.session_state.selected_student:
-        selected_student = st.session_state.selected_student
-        st.title(f"Journal Entries for {selected_student['name']}")
-        entries = db.get_journal_entries(selected_student['_id'])
-        for entry in entries:
-            st.markdown(f"**{entry['date']}**")
-            st.markdown(f"{entry['title']}")
-            st.markdown(f"{entry['content']}")
-            st.markdown(f"---")
-
 else:
     error_page_redirect()
