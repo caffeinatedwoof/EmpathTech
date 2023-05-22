43ca9b143131d7e51d3b9c5228cf977e8527f88a
Quek Zhi Qiang <zhi_qiang_quek@aiap.sg>
2023-05-23 00:01:58 +0800

    Updated colors ordering

--- pages/5_View_student_journal.py
+++ pages/5_View_student_journal.py
@@ -51,6 +51,8 @@ if 'logged_in' in st.session_state and st.session_state.logged_in:
         
         selected_student_name = st.selectbox("Student name", student_names_id_dict.values())
         st.session_state.current_student_name = selected_student_name
+
+
     key_list = list(student_names_id_dict.keys())
     value_list = list(student_names_id_dict.values())
 
@@ -60,32 +62,55 @@ if 'logged_in' in st.session_state and st.session_state.logged_in:
     #Iterator cursor output
     entries = db.get_journal_entries(key_list[selected_student_id])
     
-    # Consolidate results and reverse their order. Not needed if we can use pymongo to sort based on dates itself
-    entries_list = [i for i in entries][::-1]
+    # Pymongo has sorted order
+    entries_list = [i for i in entries]
     
     ####
     #Emotion history
     ####
     st.subheader("Emotion history")
     emotion_list = ['Positive', 'Neutral', 'Negative']
-    sent_dict = dict(zip([2, 1, 0],emotion_list))
-    time_periods = [entry['date'] for entry in entries_list[::-1]]
+    sent_dict = dict(zip([2, 1, 0], emotion_list))
+
+    # Reverse order to plot
+    time_periods = [entry['date'] for entry in entries_list]
+
+
     sentiment_list = []
     for entry in entries_list:
         sentiment = db.get_summary(entry['_id'])['sentiment']['score']
-        sentiment_list.append(sent_dict[sentiment])
+        sentiment_list.append(sentiment)
+        #sentiment_list.append(sent_dict[sentiment])
     
     # emotions = [random.choice(emotion_list) for _ in time_periods]
+    color_dict = {'Negative': 'red',
+                'Neutral': 'grey',
+                'Positive': 'green',
+                }
+    fig = px.scatter(x=time_periods, 
+                     y=sentiment_list,
+                     color=sentiment_list,
+                     )
+    
+    fig.update_layout(xaxis_title='Time',
+                      yaxis_title=None,
+                      yaxis_range=[-0.5,2.5],
+                      yaxis = dict(
+                                tickmode = 'array',
+                                tickvals = [0, 1, 2],
+                                ticktext = emotion_list[::-1]
+                              )
+    )
 
-    fig = px.scatter(x=time_periods, y=sentiment_list)
-    fig.update_layout(xaxis_title=None, yaxis_title=None)
+    # Set marker size
+    fig.update_traces(marker=dict(size=15))
     st.plotly_chart(fig, use_container_width=True)
 
     ####
     #Journal entries
     ####
     st.subheader(f"Journal entries by {selected_student_name}")
-    # Iterate cursor
+
     counter = 0
     for entry in entries_list:
         subcol1, subcol2, subcol3, subcol4= st.columns([0.5,2,0.5,0.8])
@@ -104,7 +129,9 @@ if 'logged_in' in st.session_state and st.session_state.logged_in:
         with subcol3:
             if counter == 0:
                 st.markdown("**Emotion**")
-            sentiment = db.get_summary(entry['_id'])['sentiment']['score']
+
+            # Converted sentiment from value to string
+            sentiment = sent_dict[db.get_summary(entry['_id'])['sentiment']['score']]
             st.markdown(f"<div align='center'>{sentiment}</div>", unsafe_allow_html=True)
         with subcol4:
             if counter == 0:
