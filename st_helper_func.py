6c5bd6cda42dc06be8d64f1587059aa09e547866
Zhi Qiang Quek <zhi_qiang_quek@aiap.sg>
2023-05-23 10:57:07 +0800

    Remove unused import

--- st_helper_func.py
+++ st_helper_func.py
@@ -5,7 +5,6 @@ import json
 import streamlit as st
 import toml
 import dbhandler
-from st_aggrid import GridOptionsBuilder, AgGrid
 from streamlit.source_util import _on_pages_changed, get_pages
 from streamlit_extras.switch_page_button import switch_page
 
